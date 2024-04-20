756fb109704406ce764fad57b5889af3468bc0b6
Joyce Zou <jzou771@gmail.com>
2024-04-20 15:39:25 -0400

    leaderboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg'; // Fix the import path for viteLogo
 import './App.css';
 import ReactDOM from 'react-dom';
+import Leaderboard from './components/Leaderboard.jsx';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -19,6 +20,7 @@ const App = () => {
        <Routes>
           <Route path="/" element={<StudyGroup />} />
           <Route path="/studygroup" element={<StudyGroup />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
       </div>
     </Router>
--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,14 +1,48 @@
 /* eslint-disable no-unused-vars */
 import React, { useState , useEffect} from 'react';
-import User from '../../../backend/models/user.model';
+import MockUsers from './mockUsers';
+import { set } from 'firebase/database';
+import './Leaderboard.css';
 
 const Leaderboard = () => {
-    const [studytime, setStudyTime] = useState([]);
+    const [leaderboard, setLeaderboard] = useState([]);
     
-    useEffect(()=>{
-        
+    useEffect(() => {
+        const sortedUsers = MockUsers.sort((a, b) => b.studyTime - a.studyTime);
+        const extractInfo = sortedUsers.map((user,index) => ({
+            name: user.username,
+            time: user.studyTime,
+            avatar: user.avatar,
+            rank: index + 1
+        }));
+        setLeaderboard(extractInfo);
+    }, [MockUsers]); //depends on user data
 
-    })
+    return(
+        <div className='leaderboard-container'>
+            <h2>Leaderboard</h2>
+            <div className='rank-container'>  
+                {leaderboard.map(user => (
+                    <div className='individuals' key={user}>
+                        <div className='individual-info'> 
+                            <span>{user.rank}   </span>
+                            <span>{user.avatar}   </span>
+                            <span>{user.name}   </span>
+                            <span>{user.time} minutes</span>
+                        </div>
+                      
+                        
+                    </div>
+                ))}
+            
+            </div>
+
+
+        </div>
+    )
+
+
+
 
 }
 export default Leaderboard;
