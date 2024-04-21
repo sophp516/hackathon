8d369550a198aaa22f228222701a57e386fc398e
rachaelhuang <rachaelrhuang@gmail.com>
2024-04-21 06:11:25 -0400

    all

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     port: 5178,
     proxy: {
-      "/api": {target: "http://localhost:2000"}
+      "/api": {target: "http://localhost:3000"}
     }
   }
 })
