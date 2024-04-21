0b0817658888765c4dcbbdb641f885b5d36747ec
rachaelhuang <rachaelrhuang@gmail.com>
2024-04-21 06:06:07 -0400

    rachael-design

--- frontend/src/navbar/navbar.jsx
+++ frontend/src/navbar/navbar.jsx
@@ -2,16 +2,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css'
+import homeIcon from '../assets/icon-home.svg';
+import studyGroupIcon from '../assets/icon-groups.svg';
+import studyStatsIcon from '../assets/icon-stats.svg';
+import avatarIcon from '../assets/icon-avatar.svg';
+
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <p className="studyMates">StudyMates</p>
       <div className="navlink-container">
-      <NavLink  style={{ textDecoration: 'none' }} to="/" ><div className="link-nav first">Home</div></NavLink>
-      <NavLink  style={{ textDecoration: 'none' }} to="/studybase" ><div className="link-nav">Study Group</div></NavLink>
-      <NavLink  style={{ textDecoration: 'none' }} to="/studybase" ><div className="link-nav">Study Stats</div></NavLink>
-      <NavLink  style={{ textDecoration: 'none' }} to="/" ><div className="link-nav">My Avatar</div></NavLink>
+      <NavLink  style={{ textDecoration: 'none' }} to="/" >
+        <div className="link-nav first">
+        <img src={homeIcon} alt="Home" className="nav-icon" />
+          Home</div>
+
+        </NavLink>
+        <NavLink  style={{ textDecoration: 'none' }} to="/studybase" >
+        <div className="link-nav">
+        <img src={studyGroupIcon} className="nav-icon" />
+          Study Group</div>
+
+        </NavLink>
+        <NavLink  style={{ textDecoration: 'none' }} to="/studybase" >
+        <div className="link-nav">
+        <img src={studyStatsIcon} alt="Study Stats" className="nav-icon" />
+          Study Stats</div>
+
+        </NavLink>
+        <NavLink  style={{ textDecoration: 'none' }} to="/" >
+        <div className="link-nav">
+        <img src={avatarIcon} alt="My Avatar" className="nav-icon" />
+        My Avatar</div>
+        </NavLink>
       </div>
 
     </div>
