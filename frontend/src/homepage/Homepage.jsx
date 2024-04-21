8482ee983710c88e84b8da1bfa8457702e572e5b
Rachael Huang <Rachael@Rachaels-Macbook.local>
2024-04-21 04:15:56 -0400

    init

--- frontend/src/homepage/Homepage.jsx
+++ frontend/src/homepage/Homepage.jsx
@@ -1,30 +1,50 @@
 import useLogout from "../hooks/useLogout.js";
 import Timer from "../timer/Timer.jsx";
 import TaskManager from "../taskmanager/App.jsx";
-import useGetAvatar from "../hooks/useGetAvatar.js";
+//import useGetAvatar from "../hooks/useGetAvatar.js";
 import './Homepage.css'
 
 const Homepage = () => {
     const { logout } = useLogout();
-    const { url } = useGetAvatar();
-    const parts = url.url.toString().split("/");
-    const fileName = parts[parts.length - 1];
-    const newUrl = `/src/assets/${fileName}`;
-    console.log(url)
 
     const LogoutButton = () => {
         return (
             <div>
-                <button onClick={logout}>log out</button>
+                <button className="homepage-button"onClick={logout}>log out</button>
             </div>
         )
     }
     return (
         <div className="homepage-main-container">
-            <img src={newUrl} />
-            <Timer />
-            <TaskManager />
-            <LogoutButton />
+            /* Nav bar here */
+            <div className="homepage-content">
+                <div className="left-column">
+                    <h1 className="homepage-header">
+                        <span>W</span>
+                        <span>e</span>
+                        <span>l</span>
+                        <span>c</span>
+                        <span>o</span>
+                        <span>m</span>
+                        <span>e</span>
+                        <span> </span>
+                        <span>b</span>
+                        <span>a</span>
+                        <span>c</span>
+                        <span>k</span>
+                        <span>!</span>
+                        </h1> {/* Welcome message here */}
+                    {/* <h1 className="homepage-header">Welcome back!</h1> Welcome message here */}
+                    <h3 className="homepage-text">So glad you're here. Seize the day and get some studying done.</h3> {/* Text here */}
+                        <img src="src/assets/default.png" />
+                        / * Study Group component * /
+                </div>
+                <div className="right-column">
+                    <Timer className="timer-component" />
+                    <TaskManager />
+                    <LogoutButton />
+                </div>
+            </div>
         </div>
 
     )
