fe0cc71f60d3076e83e5b623da10728e822c467c
Sophie Park <sophie.park@dali.dartmouth.edu>
2024-04-21 05:18:12 -0400

    current

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     group: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Group",
-        // default: ''
+
     },
     studyTime: {
         type: Number,
@@ -31,8 +31,8 @@ const userSchema = new mongoose.Schema({
     },
     task: [
         {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Task"
+            type: String,
+            default: []
         }
     ],
     coordinates: {
